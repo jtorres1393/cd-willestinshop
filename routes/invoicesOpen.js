9e77701cbc1d0edf9ff83c382cc8db25dbc8a48f
Ro Oeurn <rortha@gmail.com>
2020-04-02 16:17:21 -0700

    new

--- routes/invoicesOpen.js
+++ routes/invoicesOpen.js
@@ -3,7 +3,6 @@ const { Invoices } = require('../models/invoices')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-  const currId = req.query.id
   const data = await Invoices.query()
           .whereNot('status','paid')
           .eager('buyer')
