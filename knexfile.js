6b0ab88f4334e541ebd1ad4a8b797d5c59acbcb6
Ro Oeurn <rortha@gmail.com>
2020-05-09 11:50:47 -0700

    new

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,14 @@ module.exports = {
     development:{
     client: 'pg',
     connection: 'postgres://localhost:5432/willes',
-    useNullAsDefault: true
+    pool: {
+        afterCreate: function (conn, done) {
+          // in this example we use pg driver's connection API
+          conn.query('SET TIMEZONE="America/Los_Angeles";', function (err) {
+            done(err, conn);
+          });
+        }
+      }
     },
     test:{
      client: 'pg',
@@ -15,7 +22,14 @@ module.exports = {
     production: {
         client: 'pg',
         connection: process.env.DATABASE_URL,
-        useNullAsDefault: true
+        pool: {
+            afterCreate: function (conn, done) {
+              // in this example we use pg driver's connection API
+              conn.query('SET TIMEZONE="America/Los_Angeles";', function (err) {
+                done(err, conn);
+              });
+            }
+          }
 
     }
 }
